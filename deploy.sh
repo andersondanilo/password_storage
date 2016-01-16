433f8b755ca7648ac1f5ec81a5ec6c84a3379ff9
Anderson Danilo <contato@andersondanilo.com>
2016-01-16 20:17:44 -0200

    alterações

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
+git checkout master
+
 git branch -D gh-pages
-git branch -D heroku-master
-
+git checkout gh-pages-production
 git branch gh-pages
-git branch heroku-master
-
 git filter-branch -f --subdirectory-filter client -- gh-pages
-git filter-branch -f --subdirectory-filter api -- heroku-master
 
-# git push origin --delete gh-pages
-# git push heroku --delete master
+git checkout master
+git branch -D heroku-master
+git branch heroku-master
+git filter-branch -f --subdirectory-filter api -- heroku-master
 
 git push origin gh-pages
 git push heroku heroku-master:master
