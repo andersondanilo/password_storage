a723d458b760bca6de38cd53c2dba15808167b21
Anderson Danilo <contato@andersondanilo.com>
2016-01-16 19:19:53 -0200

    script deploy

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,8 @@ git branch heroku-master
 git filter-branch -f --subdirectory-filter client -- gh-pages
 git filter-branch -f --subdirectory-filter api -- heroku-master
 
-git push origin --delete gh-pages
-git push heroku --delete master
+# git push origin --delete gh-pages
+# git push heroku --delete master
 
 git push origin gh-pages
 git push heroku heroku-master:master
